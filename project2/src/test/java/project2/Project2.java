b25d0892c1a6c9bcb4ee4409fc7734cd86055434
Ravish Kumar <ravish.kumar@chromium.org>
2025-05-01 21:28:18 +0530

    Added tests

--- project2/src/test/java/project2/Project2.java
+++ project2/src/test/java/project2/Project2.java
@@ -4,17 +4,15 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
+import java.util.List;
 
 public class Project2 {
 
-    @Test
-    public void test1() {
+    public void bookFlight(String fromCity, String toCity) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
@@ -23,55 +21,154 @@ public class Project2 {
         try {
             driver.get("https://www.goibibo.com/flights/");
 
-            // Close popup
-            WebElement closePopup = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='logSprite icClose']")));
-            closePopup.click();
+            // Close popup if present
+            try {
+                WebElement closePopup = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='logSprite icClose']")));
+                closePopup.click();
+            } catch (Exception e) {
+                System.out.println("Popup not found or already closed.");
+            }
 
-            // Click on 'From' field
             WebElement fromField = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='sc-12foipm-2 eTBlJr fswFld '])[1]")));
             fromField.click();
 
-            // Enter 'Bengaluru' in from city
             WebElement fromInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//div[@class='sc-12foipm-24 bDSkOK'])/div/input")));
-            fromInput.sendKeys("Bengaluru");
+            fromInput.sendKeys(fromCity);
 
-            // Select the first suggestion
             WebElement fromSuggestion = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//p[@class='sc-12foipm-33 kUcHKT'])[1]")));
             fromSuggestion.click();
 
-            // Enter 'Patna' in to city
             WebElement toInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sc-12foipm-25 fbAAhv']/input")));
-            toInput.sendKeys("Patna");
+            toInput.sendKeys(toCity);
 
-            // Select the first suggestion
             WebElement toSuggestion = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//div[@class='sc-12foipm-29 gEpyDY'])[1]")));
             toSuggestion.click();
 
-            // Hover over departure date
             Actions action = new Actions(driver);
             WebElement dateField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='sc-zdy0j7-4 dBaZvf']")));
             action.moveToElement(dateField).perform();
 
-            // Select a date
             WebElement dateSelect = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='sc-12foipm-72 ezNmSh']")));
             dateSelect.click();
 
-            // Click on search flights
             WebElement searchFlights = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='button buttonPrimary pushRight widthFitContent']")));
             searchFlights.click();
 
-            // Wait for the results to load and click on 'View Fares'
             WebElement viewFares = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//span[text()='VIEW FARES'])[1]")));
             viewFares.click();
 
-            // Click on 'Book Now'
             WebElement bookNow = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='lato-black button buttonPrimary buttonBig fontSize14'][1]")));
             bookNow.click();
 
+            System.out.println(" Flight booking test passed for: " + fromCity + " to " + toCity);
+
         } catch (Exception e) {
+            System.out.println(" Flight booking failed for: " + fromCity + " to " + toCity);
             e.printStackTrace();
         } finally {
             driver.quit();
         }
     }
-}
+
+    @Test(priority = 1)
+    public void testBangaloreToPatna() {
+        bookFlight("Bengaluru", "Patna");
+    }
+
+    @Test(priority = 2)
+    public void testMumbaiToDelhi() {
+        bookFlight("Mumbai", "Delhi");
+    }
+
+//    @Test(priority = 3)
+//    public void testHyderabadToKolkata() {
+//        bookFlight("Hyderabad", "Kolkata");
+//    }
+//
+//    @Test(priority = 4)
+//    public void testChennaiToJaipur() {
+//        bookFlight("Chennai", "Jaipur");
+//    }
+//
+//    @Test(priority = 5)
+//    public void testPuneToAhmedabad() {
+//        bookFlight("Pune", "Ahmedabad");
+//    }
+
+    @Test(priority = 6)
+    public void testHomePageElements() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+
+        try {
+            driver.get("https://www.goibibo.com/");
+
+            // Wait for the page to load and close any popups if present
+            try {
+                WebElement closePopup = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='logSprite icClose']")));
+                closePopup.click();
+            } catch (Exception e) {
+                System.out.println("Popup not found or already closed.");
+            }
+
+            // Check visibility and interactability of tabs (Flights, Hotels, Trains, etc.)
+            
+            String[] xpaths = {
+                "//a[@href='/flights/']",
+                "//a[@href='/hotels/']",
+                "//a[@href='/trains/']",
+                "//a[@href='/cars/']",
+                "//a[@href='/bus/']",
+                "//a[@href='https://tripmoney.goibibo.com/partners/gi/forex/listing?utm_source=header_goibibo&utm_medium=desktop']",
+                "//a[@href='/giholidays/']",
+                "//a[@href='/insurance?cmp=desktop_header']",
+                "//a[@href='/travelhistory']",
+                
+               
+            };
+
+            for (int i = 0; i < xpaths.length; i++) {
+                try {
+                    WebElement el = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpaths[i])));
+                    if (el.isDisplayed() && el.isEnabled()) {
+                        System.out.println(" " + el.getText() + " tab is visible and clickable.");
+                    } else {
+                        System.out.println(" " + el.getText() + " tab is not interactable.");
+                    }
+                } catch (Exception e) {
+                    System.out.println(" " + xpaths[i] + " tab failed due to: " + e.getMessage());
+                }
+            }
+
+            // Check Login button
+            try {
+                WebElement loginBtn = driver.findElement(By.id("get_sign_in"));
+                if (loginBtn.isDisplayed()) {
+                    System.out.println(" Login button is visible.");
+                } else {
+                    System.out.println(" Login button is not visible.");
+                }
+            } catch (Exception e) {
+                System.out.println(" Login button is not visible: " + e.getMessage());
+            }
+
+         
+
+            // Check footer links
+            try {
+                List<WebElement> footerLinks = driver.findElements(By.xpath("//footer//a"));
+                System.out.println(" Footer contains " + footerLinks.size() + " links.");
+            } catch (Exception e) {
+                System.out.println(" Footer links check failed: " + e.getMessage());
+            }
+
+        } catch (Exception e) {
+            System.out.println(" Homepage elements test failed due to: " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+    }
+    }
